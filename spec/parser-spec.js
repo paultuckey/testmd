e417e072413afb28da60c81e64e75108495fd847
Paul Tuckey <paul@tuckey.org>
2018-03-06 16:50:46 +1300

    pasring for the set command

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -8,7 +8,7 @@ describe("Testmd Parser", function() {
       let testmdObj = TestmdParser.parse(`
 
 note Kitchen Sink
-    note this should haver every command at least once
+    note this should have every command at least once
  note .  
         note 나는 유리를 먹을 수 있어요. 그래도 아프지 않아요
   note
@@ -18,12 +18,42 @@ note Kitchen Sink
   note2
         
         `);
-        expect(testmdObj.length).toBe(5);
-        expect(testmdObj[0].cmdType).toBe('note');
-        expect(testmdObj[0].noteValue).toBe('Kitchen Sink');
-        expect(testmdObj[2].noteValue).toBe('.');
-        expect(testmdObj[3].noteValue).toBe('나는 유리를 먹을 수 있어요. 그래도 아프지 않아요');
-        expect(testmdObj[4].noteValue).toBe('upper ok');
+        expect(testmdObj.commands.length).toBe(5);
+        expect(testmdObj.commands[0].cmdType).toBe('note');
+        expect(testmdObj.commands[0].noteValue).toBe('Kitchen Sink');
+        expect(testmdObj.commands[2].noteValue).toBe('.');
+        expect(testmdObj.commands[3].noteValue).toBe('나는 유리를 먹을 수 있어요. 그래도 아프지 않아요');
+        expect(testmdObj.commands[4].noteValue).toBe('upper ok');
+  });
+
+  it("set parsing", function() {
+      let testmdObj = TestmdParser.parse(`
+
+set $a 234
+set $나는 유리를
+set $joiner hello $a
+set $cookie.name my cookies yum
+        
+        `);
+        expect(testmdObj.commands.length).toBe(4);
+        expect(testmdObj.allVarsUsed.length).toBe(5);
+        expect(testmdObj.commands[0].cmdType).toBe('set');
+        expect(testmdObj.commands[0].varName).toBe('$a');
+        expect(testmdObj.commands[0].varValue).toBe('234');
+        expect(testmdObj.commands[0].cmdVarsUsed.length).toBe(1);
+        expect(testmdObj.commands[0].cmdVarsUsed[0].usageVarName).toBe('$a');
+
+        expect(testmdObj.commands[1].varName).toBe('$나는');
+        expect(testmdObj.commands[1].varValue).toBe('유리를');
+        expect(testmdObj.commands[1].cmdVarsUsed.length).toBe(1);
+
+        expect(testmdObj.commands[2].varName).toBe('$joiner');
+        expect(testmdObj.commands[2].varValue).toBe('hello $a');
+        expect(testmdObj.commands[2].cmdVarsUsed.length).toBe(2);
+        expect(testmdObj.commands[2].cmdVarsUsed[0].usageVarName).toBe('$joiner');
+        expect(testmdObj.commands[2].cmdVarsUsed[1].usageVarName).toBe('$a');
+
+
   });
 
 
--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const XRegExp = require('xregexp');
 
 /**
  *
@@ -15,45 +16,93 @@ module.exports = {
 /// output a jsvscrtipt test using the template called for
 
     parse: function(testmdBlock) {
-        let commands = [];
+        let testmdObj = {
+            commands: [],
+            allVarsUsed: []
+        };
         if (!testmdBlock) testmdBlock = '';
         let lines = testmdBlock.split(/\n/);
         let lineCounter = 0;
-        lines.forEach(function(line) {
+        lines.forEach(function(rawLine) {
             lineCounter++;
-            if (!line) line = '';
-            line = line.trim();
-            let commandObj = {
-                isValid: false,  // is this command valid syntax for it's type
-                srcLineNum: lineCounter,  // the line of the file/block this came from
-
-                cmdType: null,
-
-                noteValue: null,
-                openValue: null,
-
-                objType: null,  // field/button/video
-                objSearch: null,
-
-                keysToPress: null,
-
-                varName: null,
-                checkStr: null,
-
-            };
-            if ( /^note\s+/i.test(line) ) {
-                commandObj.cmdType = 'note';
-                let noteLineMatch = /^note\s+(.*)/i.exec(line);
-                if (noteLineMatch) {
-                    commandObj.noteValue = noteLineMatch[1];
-                }
-            }
-            if (commandObj.cmdType) {
-                commands.push(commandObj);
-            }
+            module.exports.parseLine(rawLine, lineCounter, testmdObj);
         });
 
-        return commands;
+        return testmdObj;
+    },
+
+    parseLine: function(rawLine, lineCounter, testmdObj) {
+        if (!rawLine) rawLine = '';
+        let line = rawLine.trim();
+        let commandObj = {
+            isValid: false,  // is this command valid syntax for it's type
+            srcLineNum: lineCounter,  // the line of the file/block this came from
+
+            cmdType: null,
+
+            noteValue: null,
+            openValue: null,
+
+            objType: null,  // field/button/video
+            objSearch: null,
+
+            keysToPress: null,
+
+            varName: null,
+            varValue: null,
+
+            cmdVarsUsed: [],
+
+            checkStr: null,
+
+        };
+        //console.log('hoi', RegExp('^note\\s+', 'iu'), RegExp('^note\\s+', 'iu').test(line))
+        if ( XRegExp('^note\\s+', 'i').test(line) ) {
+            commandObj.cmdType = 'note';
+            let noteLineMatch = RegExp('^note\\s+(.*)$', 'i').exec(line);
+            if (noteLineMatch) {
+                commandObj.noteValue = noteLineMatch[1];
+            }
+        }
+        if ( XRegExp('^set\\s+', 'iu').test(line) ) {
+            commandObj.cmdType = 'set';
+            //console.log('w', Unicode.w);
+            // vars must start with a $ then be followed
+            let setLineMatch = XRegExp('^set\\s+\\$([\\pL0-9.]+)\\s+(.+)$', 'i').exec(line);
+            //console.log('trial', XRegExp('\\$([\\pL0-9.]+)', 'i').exec(line));
+            if (setLineMatch) {
+                commandObj.varName = '$' + setLineMatch[1];
+                commandObj.varValue = setLineMatch[2];
+
+                commandObj.cmdVarsUsed.push({
+                    usageType: 'varset', usageLineNum: commandObj.srcLineNum, usageVarName: commandObj.varName
+                });
+                let varsUsedInValue = module.exports.parseForVariables(commandObj.varValue);
+                varsUsedInValue.forEach(function(varUsedInValue) {
+                    commandObj.cmdVarsUsed.push({
+                        usageType: 'varget', usageLineNum: commandObj.srcLineNum,
+                        usageVarName: varUsedInValue
+                    });
+                });
+                commandObj.cmdVarsUsed.forEach(function(u) {
+                    testmdObj.allVarsUsed.push(u);
+                });
+            }
+        }
+        if (commandObj.cmdType) {
+            testmdObj.commands.push(commandObj);
+        }
+    },
+
+    parseForVariables: function(varValue) {
+        let varsUsed = [];
+        //let match = null;
+        let re = XRegExp('\\$([\\pL0-9.]+)', 'i');
+        XRegExp.forEach(varValue, re, function(match) {
+            //console.log('got', match);
+            varsUsed.push('$' + match[1]);
+        });
+        return varsUsed;
     }
 
 };
